555ad550ddd9f8d4d9a02408c5d90cb0b46552e6
mateopozor02 <mateopozoruiz02@hotmail.com>
2024-12-04 11:43:07 -0500

    Add formatting to files

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import { fetchRepositories } from "./api.js";
-import { displayStarredRepos, displayRecentRepos, displayTotalStars } from './ui.js';
+import {
+  displayStarredRepos,
+  displayRecentRepos,
+  displayTotalStars,
+} from "./ui.js";
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 const loadBtn = document.getElementById("loadBtn");
-loadBtn.addEventListener("click", init); 
+loadBtn.addEventListener("click", init);
 
 async function init() {
-    const repositories = await fetchRepositories();
-    displayStarredRepos(repositories);
-    displayRecentRepos(repositories);
-    displayTotalStars(repositories);
+  const repositories = await fetchRepositories();
+  displayStarredRepos(repositories);
+  displayRecentRepos(repositories);
+  displayTotalStars(repositories);
 }
